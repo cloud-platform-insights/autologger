b454bf3f3fcdd0998da1fd0b9ae4052fd6941082
Dave Stanke <davidstanke@gmail.com>
2025-02-04 12:41:04 -0500

    split out upload code

--- src/app.py
+++ src/app.py
@@ -1,20 +1,13 @@
-from flask import Flask, flash, request, redirect, render_template
-from werkzeug.utils import secure_filename
-import os
-import tempfile
+# EXPERIMENTAL Web application based on autologger
 
-ALLOWED_EXTENSIONS = {"mp4"}
+from flask import Flask, request, redirect, render_template, url_for
+
+# from flask import flash, redirect
+
+import experimental.upload as upload
 
 app = Flask(__name__)
 app.secret_key = "KbbMp_HrXRRC6se.Je-y"
-
-
-# Utility methods
-def allowed_file(filename):
-    return (
-        "." in filename
-        and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
-    )
 
 
 # Routes
@@ -23,39 +16,16 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/process", methods=["POST"])
-def process():
-    if "input_file" not in request.files:
-        flash("No file part")
-        return redirect("/")
-    file = request.files["input_file"]
-    # If the user does not select a file, the browser submits an
-    # empty file without a filename.
-    if file.filename == "":
-        flash("No selected file")
-        return redirect("/")
-    if file:
-        if not allowed_file(file.filename):
-            uploaded_file_ext = os.path.splitext(file.filename)[-1]
-            flash(
-                f"Invalid file extension: `{uploaded_file_ext}` "
-                + f"(allowed extensions: {ALLOWED_EXTENSIONS})"
-            )
-            return redirect("/")
-        else:
-            filename = secure_filename(file.filename)
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    uploaded_file_path = upload.process_upload_form(request)
+    return redirect(url_for(".process_video", file_path=uploaded_file_path))
 
-            temp_dir = tempfile.gettempdir()
-            file_destination = os.path.join(temp_dir, filename)
-            file.save(file_destination)
-            file_bytes = os.stat(file_destination).st_size
-            return (
-                f"file uploaded to: {file_destination} -- {file_bytes} bytes"
-            )
-    else:
-        raise Exception(
-            "ERROR: reached end of script without fulfilling any conditions"
-        )
+
+@app.route("/process", methods=["GET"])
+def process_video():
+    file_path = request.args.get("file_path")
+    return render_template("process.html", file_path=file_path)
 
 
 if __name__ == "__main__":
---
+++ src/experimental/upload.py
@@ -0,0 +1,50 @@
+from flask import flash, redirect
+from werkzeug.utils import secure_filename
+
+import os
+import tempfile
+
+ALLOWED_EXTENSIONS = {"mp4"}
+
+
+# Utility methods
+def allowed_file(filename):
+    return (
+        "." in filename
+        and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+    )
+
+
+def process_upload_form(request):
+    if "input_file" not in request.files:
+        flash("No file part")
+        return redirect("/")
+    file = request.files["input_file"]
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == "":
+        flash("No selected file")
+        return redirect("/")
+    if file:
+        if not allowed_file(file.filename):
+            uploaded_file_ext = os.path.splitext(file.filename)[-1]
+            flash(
+                f"Invalid file extension: `{uploaded_file_ext}` "
+                + f"(allowed extensions: {ALLOWED_EXTENSIONS})"
+            )
+            return redirect("/")
+        else:
+            filename = secure_filename(file.filename)
+
+            temp_dir = tempfile.gettempdir()
+            file_destination = os.path.join(temp_dir, filename)
+            file.save(file_destination)
+            file_size_in_bytes = os.stat(file_destination).st_size
+            print(f"Uploaded file: {file_destination}")
+            print(f"Uploaded file size: {file_size_in_bytes} bytes")
+            return file_destination
+
+    else:
+        raise Exception(
+            "ERROR: reached end of script without fulfilling any conditions"
+        )
